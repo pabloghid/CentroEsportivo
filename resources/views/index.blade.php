b3d4da5f892a84e2bd5c7f8dc526490fd679ca24
Your Name <vstrentin>
2022-05-10 07:31:58 -0300

    add telas e web_api

---
+++ resources/views/index.blade.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+  </head>
+
+  <body>
+    <div class="quadro-gramado">
+        <h1>Testes</h1>
+    </div>
+    
+    <!-- quadros -->
+    <section class="quadro-modalidade">
+      <div class="card-grid">
+        <a class="card" href='http://127.0.0.1:8000/horarios'>
+          <div class="card__background" style="background-image: url(https://i.pinimg.com/564x/87/6e/54/876e54cf2810104f9af88e6bda9a0222.jpg)"></div>
+          <div class="card__content">
+            <p class="card__category">Modalidade</p>
+            <h3 class="card__heading">Futsal</h3>
+          </div>
+        </a>
+        <a class="card" href='http://127.0.0.1:8000/horarios'>
+          <div class="card__background" style="background-image: url(https://i.pinimg.com/564x/c8/36/91/c836910ff230a5bc886ba84744b0a121.jpg)"></div>
+          <div class="card__content">
+            <p class="card__category">Modalidade</p>
+            <h3 class="card__heading">Volei</h3>
+          </div>
+        </a>
+        <a class="card" href='http://127.0.0.1:8000/horarios'>
+          <div class="card__background" style="background-image: url(https://i.pinimg.com/564x/74/0f/e1/740fe189460ad8c3798b82a97e749bb9.jpg)"></div>
+          <div class="card__content">
+            <p class="card__category">Modalidade</p>
+            <h3 class="card__heading">Basquete</h3>
+          </div>
+        </li>
+        <a class="card" href='http://127.0.0.1:8000/horarios'>
+          <div class="card__background" style="background-image: url(https://i.pinimg.com/564x/7d/38/c6/7d38c69b99e52a601c94233ffab85f95.jpg)"></div>
+          <div class="card__content">
+            <p class="card__category">Modalidade</p>
+            <h3 class="card__heading">Tenis</h3>
+          </div>
+        </a>
+      <div>
+    </section>
+      
+  </body>
+  <style>
+    :root{
+      --background-dark: #2d3548;
+      --text-light: rgba(255,255,255,0.6);
+      --text-lighter: rgba(255,255,255,0.9);
+      --spacing-s: 8px;
+      --spacing-m: 16px;
+      --spacing-l: 24px;
+      --spacing-xl: 32px;
+      --spacing-xxl: 64px;
+      --width-container: 1200px;
+    }
+
+    *{
+      border: 0;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    html{
+      height: 100%;
+      font-family: 'Montserrat', sans-serif;
+      font-size: 14px;
+    }
+
+    body{
+      height: 100%;
+    }
+/* #231F20 */
+    .quadro-modalidade{
+      align-items: flex-start;
+      background-color: #231F20;
+      display: flex;
+      min-height: 100%;
+      justify-content: center;
+      padding: var(--spacing-xxl) var(--spacing-l);
+    }
+
+    .card-grid{
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      grid-column-gap: var(--spacing-l);
+      grid-row-gap: var(--spacing-l);
+      max-width: var(--width-container);
+      width: 100%;
+    }
+
+    @media(min-width: 540px){
+      .card-grid{
+        grid-template-columns: repeat(2, 1fr); 
+      }
+    }
+
+    @media(min-width: 960px){
+      .card-grid{
+        grid-template-columns: repeat(4, 1fr); 
+      }
+    }
+
+    .card{
+      list-style: none;
+      position: relative;
+      background-color: #231F20;
+      border-radius: 100cm;
+    }
+
+    .card:before{
+      content: '';
+      display: block;
+      padding-bottom: 150%;
+      width: 100%;
+      background-color: #231F20;
+    }
+
+    .card__background{
+      background-color: #231F20;
+      background-size: cover;
+      background-position: center;
+      border-radius: var(--spacing-l);
+      bottom: 0;
+      filter: brightness(0.75) saturate(1.2) contrast(0.85);
+      left: 0;
+      position: absolute;
+      right: 0;
+      top: 0;
+      transform-origin: center;
+      transform: scale(1) translateZ(0);
+      transition: 
+        filter 200ms linear,
+        transform 200ms linear;
+    }
+
+    .card:hover .card__background{
+      transform: scale(1.05) translateZ(0);
+    }
+
+    .card-grid:hover > .card:not(:hover) .card__background{
+      filter: brightness(0.5) saturate(0) contrast(1.2) blur(20px);
+    }
+
+    .card__content{
+      left: 0;
+      padding: var(--spacing-l);
+      position: absolute;
+      top: 0;
+    }
+
+    .card__category{
+      color: var(--text-light);
+      font-size: 0.9rem;
+      margin-bottom: var(--spacing-s);
+      text-transform: uppercase;
+    }
+
+    .card__heading{
+      color: var(--text-lighter);
+      font-size: 1.9rem;
+      text-shadow: 2px 2px 20px rgba(0,0,0,0.2);
+      line-height: 1.4;
+      word-spacing: 100vw;
+    }
+
+    .quadro-gramado{
+        display: grid;
+        width: 100%;
+        background-image: url(https://img.freepik.com/fotos-gratis/linha-de-futebol-na-grama-verde-do-campo-de-futebol-fundo-de-campo-gramado-verde_64749-2453.jpg);
+    }
+
+  </style>
+</html> 
---
+++ resources/views/selecHorario.blade.php
@@ -0,0 +1,494 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seleciona Horario</title>
+
+    </head>
+<body>
+
+    <div class="row">
+        <div class="col-4">
+            <div class="container">
+                <div class="calendar-assets">
+                    <h1 id="currentDate"></h1>
+                    <div class="field">
+                        <label for="date">Pesquisa por data</label>
+                        <form class="form-input" id="date-search" onsubmit="setDate(this)">
+                            <input type="date" class="text-field" name="date" id="date" required>
+                            <button type="submit" class="btn btn-small" title="Pesquisar"><i class="fas fa-search"></i></button>
+                        </form>
+                    </div>
+                    <div class="day-assets">
+                        <button class="btn" onclick="prevDay()" title="Dia anterior"><i class="fas fa-chevron-left"></i> </button>
+                        <button class="btn" onclick="resetDate()" title="Dia atual"><i class="fas fa-calendar-day"></i> Hoje</button>
+                        <button class="btn" onclick="nextDay()" title="Próximo dia"><i class="fas fa-chevron-right"></i> </button>
+                    </div>
+                </div>
+                <div class="calendar" id="table">
+                    <div class="header">
+                        <!-- Aqui é onde ficará o h1 com o mês e o ano -->
+                        <div class="month" id="month-header">
+        
+                        </div>
+                        <div class="buttons">
+                            <button class="icon" onclick="prevMonth()" title="Mês anterior"><i class="fas fa-chevron-left"></i></button>
+                            <button class="icon" onclick="nextMonth()" title="Próximo mês"><i class="fas fa-chevron-right "></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+            <script src="https://kit.fontawesome.com/812e771e48.js" crossorigin="anonymous"></script>
+
+        </div>
+        
+</body>
+
+<style>
+    * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Open Sans", sans-serif;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+}
+
+input,
+button,
+textarea {
+    font-family: "Open Sans", sans-serif;
+    font-size: 14px;
+}
+
+
+/* Basis */
+
+.btn {
+    font-family: 'Open Sans';
+    font-size: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: #fff;
+    color: #262626;
+    border-radius: 10px;
+    height: 50px;
+    font-weight: 600;
+    min-width: 100px;
+    padding: 0px 20px;
+    transition: 0.2s ease;
+    cursor: pointer;
+    outline: 0;
+}
+
+.btn:hover {
+    background: #455AF7;
+}
+
+.btn-small {
+    min-width: 50px;
+}
+
+
+.btn-day {
+    font-family: 'Open Sans';
+    font-size: 1em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    background: rgba(0, 0, 0, 0);
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    z-index: 99;
+    outline: 0;
+    border-radius: 10px;
+    transition: 0.4s all;
+}
+
+.btn-day.active {
+    color: #FFF;
+    font-weight: 600;
+    z-index: 99;
+}
+
+.btn-day.active::before {
+    position: absolute;
+    content: '';
+    width: 40px;
+    height: 40px;
+    background: #455AF7;
+    border-radius: 10px;
+    z-index: -1;
+}
+
+.text-field {
+    padding: 10px;
+    height: 50px;
+    width: 200px;
+    border: 1px solid #f0f0f0;
+    border-radius: 10px;
+    margin: 0px 10px 0 0;
+}
+
+.title {
+    font-size: 1.3em;
+}
+
+.subtitle {
+    font-size: 1.1em;
+}
+
+/* Calendar */
+
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    min-height: 100vh;
+    background-color: fuchsia;
+
+}     
+
+
+.calendar-assets {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    padding: 10px;
+}
+
+#currentDate{
+  text-align:center;
+  font-size:20px;
+}
+
+.field {
+    display: flex;
+    width: auto;
+    flex-direction: column;
+    align-items: left;
+    justify-content: center;
+    margin: 20px 0;
+}
+
+.field label {
+    margin: 2px;
+}
+
+.form-input {
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+}
+
+.day-assets {
+    display: flex;
+    justify-content: space-between;
+    width: 324px;
+}
+
+
+
+.day-assets .btn:nth-child(2) i {
+    padding: 0 10px 0 0 !important;
+}
+
+.calendar {
+    height: 370px;
+    width: 324px;
+    border-radius: 18px;
+    background: #ffffff;
+	  padding: 10px;
+	  user-select: none;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-radius: 18px 18px 0px 0px;
+    padding: 15px;
+    height: 50px;
+    width: 100%;
+    color: #262626;
+}
+
+.month {
+    font-size: 0.7em;
+}
+
+.icon {
+    padding: 10px;
+    font-size: 1em;
+    margin: 0 0 0 5px;
+    background: #ffffff;
+    color: #455af7;
+    border: none;
+    cursor: pointer;
+    outline: 0;
+}
+
+.icon:hover {
+    cursor: pointer;
+}
+
+#calendar {
+    width: 100%;
+}
+
+.weekends th {
+    font-size: 1em;
+    color: gray;
+    font-weight: 400;
+    width: 40px;
+    margin: 10px 0;
+}
+
+#calendar tr {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
+    margin: 3px;
+}
+
+#calendar td {
+    display: flex;
+    height: 40px;
+    width: 40px;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    z-index: 99;
+}
+</style>
+
+<script>
+    const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+];
+
+const weekdays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+
+
+
+// Váriavel principal
+let date = new Date();
+
+// Função que retorna a data atual do calendário 
+function getCurrentDate(element, asString) {
+    if (element) {
+        if (asString) {
+            return element.textContent = weekdays[date.getDay()] + ', ' + date.getDate() + " de " + months[date.getMonth()] + " de " + date.getFullYear();
+        }
+        return element.value = date.toISOString().substr(0, 10);
+    }
+    return date;
+}
+
+
+// Função principal que gera o calendário
+function generateCalendar() {
+
+    // Pega um calendário e se já existir o remove
+    const calendar = document.getElementById('calendar');
+    if (calendar) {
+        calendar.remove();
+    }
+
+    // Cria a tabela que será armazenada as datas
+    const table = document.createElement("table");
+    table.id = "calendar";
+
+    // Cria os headers referentes aos dias da semana
+    const trHeader = document.createElement('tr');
+    trHeader.className = 'weekends';
+    weekdays.map(week => {
+        const th = document.createElement('th');
+        const w = document.createTextNode(week.substring(0, 3));
+        th.appendChild(w);
+        trHeader.appendChild(th);
+    });
+
+    // Adiciona os headers na tabela
+    table.appendChild(trHeader);
+
+    //Pega o dia da semana do primeiro dia do mês
+    const weekDay = new Date(
+        date.getFullYear(),
+        date.getMonth(),
+        1
+    ).getDay();
+
+    //Pega o ultimo dia do mês
+    const lastDay = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        0
+    ).getDate();
+
+    let tr = document.createElement("tr");
+    let td = '';
+    let empty = '';
+    let btn = document.createElement('button');
+    let week = 0;
+
+    // Se o dia da semana do primeiro dia do mês for maior que 0(primeiro dia da semana);
+    while (week < weekDay) {
+        td = document.createElement("td");
+        empty = document.createTextNode(' ');
+        td.appendChild(empty);
+        tr.appendChild(td);
+        week++;
+    }
+
+    // Vai percorrer do 1º até o ultimo dia do mês
+    for (let i = 1; i <= lastDay;) {
+        // Enquanto o dia da semana for < 7, ele vai adicionar colunas na linha da semana
+        while (week < 7) {
+            td = document.createElement('td');
+            let text = document.createTextNode(i);
+            btn = document.createElement('button');
+            btn.className = "btn-day";
+            btn.addEventListener('click', function () { changeDate(this) });
+            week++;
+
+
+
+            // Controle para ele parar exatamente no ultimo dia
+            if (i <= lastDay) {
+                i++;
+                btn.appendChild(text);
+                td.appendChild(btn)
+            } else {
+                text = document.createTextNode(' ');
+                td.appendChild(text);
+            }
+            tr.appendChild(td);
+        }
+        // Adiciona a linha na tabela
+        table.appendChild(tr);
+
+        // Cria uma nova linha para ser usada
+        tr = document.createElement("tr");
+
+        // Reseta o contador de dias da semana
+        week = 0;
+    }
+    // Adiciona a tabela a div que ela deve pertencer
+    const content = document.getElementById('table');
+    content.appendChild(table);
+    changeActive();
+    changeHeader(date);
+    document.getElementById('date').textContent = date;
+    getCurrentDate(document.getElementById("currentDate"), true);
+    getCurrentDate(document.getElementById("date"), false);
+}
+
+// Altera a data atráves do formulário
+function setDate(form) {
+    let newDate = new Date(form.date.value);
+    date = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate() + 1);
+    generateCalendar();
+    return false;
+}
+
+// Método Muda o mês e o ano do topo do calendário
+function changeHeader(dateHeader) {
+    const month = document.getElementById("month-header");
+    if (month.childNodes[0]) {
+        month.removeChild(month.childNodes[0]);
+    }
+    const headerMonth = document.createElement("h1");
+    const textMonth = document.createTextNode(months[dateHeader.getMonth()].substring(0, 3) + " " + dateHeader.getFullYear());
+    headerMonth.appendChild(textMonth);
+    month.appendChild(headerMonth);
+}
+
+// Função para mudar a cor do botão do dia que está ativo
+function changeActive() {
+    let btnList = document.querySelectorAll('button.active');
+    btnList.forEach(btn => {
+        btn.classList.remove('active');
+    });
+    btnList = document.getElementsByClassName('btn-day');
+    for (let i = 0; i < btnList.length; i++) {
+        const btn = btnList[i];
+        if (btn.textContent === (date.getDate()).toString()) {
+            btn.classList.add('active');
+        }
+    }
+}
+
+// Função que pega a data atual
+function resetDate() {
+    date = new Date();
+    generateCalendar();
+}
+
+// Muda a data pelo numero do botão clicado
+function changeDate(button) {
+    let newDay = parseInt(button.textContent);
+    date = new Date(date.getFullYear(), date.getMonth(), newDay);
+    generateCalendar();
+}
+
+// Funções de avançar e retroceder mês e dia
+function nextMonth() {
+    date = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    generateCalendar(date);
+}
+
+function prevMonth() {
+    date = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    generateCalendar(date);
+}
+
+
+function prevDay() {
+    date = new Date(date.getFullYear(), date.getMonth(), date.getDate() - 1);
+    generateCalendar();
+}
+
+function nextDay() {
+    date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+    generateCalendar();
+}
+
+document.onload = generateCalendar(date);
+
+</script>
+
+</html>
