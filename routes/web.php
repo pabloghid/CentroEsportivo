e58d269759412f16bbee98fd83658d4fe192b7f3
Your Name <vstrentin>
2022-05-10 07:33:04 -0300

    add web_php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/horarios', function () {
+    return view('selecHorario');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro');
 });
